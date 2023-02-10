d784b7224b12705d5417edb8d074b33d2824865e
Jonald Damaso <damasojonald@gmail.com>
2023-02-10 11:31:41 +0000

    feat: added Right Turn and updated ReadMe

--- README.md
+++ README.md
@@ -1,15 +1,21 @@
 # Mars Rover Tech Task 
 ## Pyramid Diagram / UML
-![Pyramid Diagram](./media/Pryamid%20Diagram-Mars%20Rover.pdf);
+! [Pyramid Diagram](./media/Pryamid%20Diagram-Mars%20Rover.pdf);
 
 
 ## Understand the Movement and Co-ordinates and Plateau
-### Turning Left
-N => W,
-W => S,
-S => E,
-S => E,
-E => N
+### Turning Left      
+- N => W,
+- W => S,
+- S => E,
+- S => E,
+- E => N
+
+### Turning Right
+- N => E,
+- E => S,
+- S => W,
+- W => N
 
 
 
--- src/mars-rover.test.ts
+++ src/mars-rover.test.ts
@@ -4,6 +4,12 @@ const turnLeft = (x:string) => {
     if(x === "S") return "E";
     return "N";
 };
+const turnRight = (x:string) => {
+    if(x === "N") return "E";
+    if(x === "E") return "S";
+    if(x === "S") return "W";
+    return "N";
+};
 
 test.each`
 original | expected
@@ -14,4 +20,14 @@ ${"E"}   | ${"N"}
 `(
     "When facing $original, turning left should cause us to face $expected",
     ({ original, expected}) => expect(turnLeft(original)).toBe(expected)
+);
+test.each`
+original | expected
+${"N"}   | ${"E"}
+${"E"}   | ${"S"}
+${"S"}   | ${"W"}
+${"W"}   | ${"N"}
+`(
+    "When facing $original, turning left should cause us to face $expected",
+    ({ original, expected}) => expect(turnRight(original)).toBe(expected)
 );
