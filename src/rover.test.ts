7012165812e0b46b1c19fc8213b899d633123238
Jonald Damaso <damasojonald@gmail.com>
2023-02-13 18:06:51 +0000

    just for temporary solutions for now, I need to check bit by bit

--- src/rover.test.ts
+++ src/rover.test.ts
@@ -1,53 +1,27 @@
-type Heading = "N" | "W" | "S" | "E";
-type Coordinates = [x: number, y: number];
+import { run } from "./rover";
 
-const Compass : Array<Heading> = ["N", "E", "S", "W"];
-
-const rotate = (turns: number) => (heading: Heading) => {
-    const idx = Compass.indexOf(heading);
-    return Compass[(idx + turns) % 4]
-}
-const turnLeft = rotate(3);
-const turnRight = rotate(1);
-const move = (heading: Heading, position: Coordinates) => {
-    const [x, y] = position;
-    if (heading === "N") return [x, y + 1];
-    if (heading === "E") return [x + 1, y];
-    if (heading === "S") return [x, y - 1];
-    if (heading === "W") return [x - 1, y];
-};
-
-test.each`
-original | expected
-${"N"}   | ${"W"}
-${"W"}   | ${"S"}
-${"S"}   | ${"E"}
-${"E"}   | ${"N"}
-`(
-    "When facing $original, turning left should cause us to face $expected",
-    ({ original, expected}) => expect(turnLeft(original)).toBe(expected)
-);
-test.each`
-original | expected
-${"N"}   | ${"E"}
-${"E"}   | ${"S"}
-${"S"}   | ${"W"}
-${"W"}   | ${"N"}
-`(
-    "When facing $original, turning right should cause us to face $expected",
-    ({ original, expected}) => expect(turnRight(original)).toBe(expected)
-);
-
-test("When moving N, we should increment the Y coordinate", () => {
-    expect(move("N", [1, 1])).toEqual([1,2]);
-});
-test("When moving E, we should increment the X coordinate", () => {
-    expect(move("E", [1, 1])).toEqual([2,1]);
+test("When executing a program", () => {
+    const program = ["5 5", "1 2 N", "LMLMLMLMM", "3 3 E", "MMRMMRMRRM"];
+    expect(run(program)).toEqual(["1 3 N", "5 1 E"]);
 });
 
-test("When moving S, we should decrement the Y coordinate", () => {
-    expect(move("S", [1, 1])).toEqual([1,0]);
+test("When moving west off the edge of the grid", () =>{
+    const program = ["0 0", "0 0 W", "M"];
+    expect(run(program)).toEqual(["F 0 0 W"]);
 });
-test("When moving W, we should decrement the X coordinate", () => {
-    expect(move("W", [1, 1])).toEqual([0,1]);
+test("When moving south off the edge of the grid", () =>{
+    const program = ["0 0", "0 0 S", "M"];
+    expect(run(program)).toEqual(["F 0 0 S"]);
+});
+test("When moving north off the edge of the grid", () =>{
+    const program = ["0 0", "0 0 N", "M"];
+    expect(run(program)).toEqual(["F 0 0 N"]);
+});
+test("When moving east off the edge of the grid", () =>{
+    const program = ["0 0", "0 0 E", "M"];
+    expect(run(program)).toEqual(["F 0 0 E"]);
+});
+test("When moving east off the edge of the grid", () =>{
+    const program = ["0 0", "0 0 N", "MLLM"];
+    expect(run(program)).toEqual(["F 0 0 N"]);
 });
